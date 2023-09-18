898d601162a82fe63d346208085bfa84f22e2cb1
Dan Oved <stangogh@gmail.com>
2023-09-18 14:52:50 -0700

    Delete some tests

--- src/counter.test.ts
+++ src/counter.test.ts
@@ -29,27 +29,3 @@ const publicClient = createPublicClient({
   chain: foundry,
   transport: http(),
 });
-
-it("it can deploy the counter contract WITHOUT external libs", async () => {
-  const hash = await walletClient.deployContract({
-    abi: counterABI,
-    account: deployerAccount,
-    bytecode: counterJson.bytecode.object as `0x${string}`
-  });
-
-  const receipt = await publicClient.waitForTransactionReceipt({ hash });
-
-  expect(receipt.status).toBe('success');
-});
-
-it("it can deploy the counter contract WITH external libs", async () => {
-  const hash = await walletClient.deployContract({
-    abi: counterUsingLibABI,
-    account: deployerAccount,
-    bytecode: counterUsingLibJson.bytecode.object as `0x${string}`
-  });
-
-  const receipt = await publicClient.waitForTransactionReceipt({ hash });
-
-  expect(receipt.status).toBe('success');
-});
