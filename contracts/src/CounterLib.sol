757a867bf0d8882eded1a90ebd368dac6b6d6858
Dan Oved <stangogh@gmail.com>
2023-09-18 14:48:58 -0700

    inc by 4

--- contracts/src/CounterLib.sol
+++ contracts/src/CounterLib.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.13;
 
 library CounterLib {
     function increment(uint256 number) external pure returns (uint256) {
-        return number + 1;
+        return number + 4;
     }
 }
